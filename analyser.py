438cb65edebfb49c813a791b9beb03a5af0ea9de
TomLiDev <t.litchfield00@gmail.com>
2024-07-22 15:09:27 +0100

    Add code to create list of dataframes from CSV data

--- analyser.py
+++ analyser.py
@@ -1,5 +1,47 @@
 import pandas as pd
 
-file = pd.read_csv('testpricedatashort_csv.csv')
+file = pd.read_csv('testpricedatashort_csv.csv', nrows=300)
+data = pd.DataFrame(file)
 
-print(file)
+sample = (data.head(30))
+price = sample['Price']
+
+#print(sample.Price.rolling(10).min())
+
+print("thing", sample.index.size)
+sectionsTemp =  sample.index.size / 10
+sections = int(sectionsTemp)
+print("sections", sections)
+
+#test = sample.iloc[1::10, :]
+#print("test", test)
+
+#print("Sample2", sample2)
+#print("Test", sample.loc[4:5])
+
+splitSections = []
+
+#newDf = pd.DataFrame(data, 30)
+#print("TEST", newDf)
+
+for x in range(sections):
+    if x != 0:
+        end = x*10
+        print("this is x", x*10)
+        piece = sample.loc[end-10: end]
+        pd.DataFrame(piece)
+        splitSections.append(piece)
+        print("Inside loop", piece)
+else: 
+    print("pass")
+    pass
+
+print("working?", splitSections)
+    
+
+
+
+#for index, row in sample2.iterrows():
+    #print("inside function", row['Price'], row['Time (local)'])
+
+#chopper()
