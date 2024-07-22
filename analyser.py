8ddca17582c91ff25164e6092909d8d0d31f14ad
TomLiDev <t.litchfield00@gmail.com>
2024-07-22 15:37:21 +0100

    Edit code to create dict of dataframes from CSV data

--- analyser.py
+++ analyser.py
@@ -3,45 +3,34 @@ import pandas as pd
 file = pd.read_csv('testpricedatashort_csv.csv', nrows=300)
 data = pd.DataFrame(file)
 
-sample = (data.head(30))
+sample = (data.head(50))
 price = sample['Price']
 
-#print(sample.Price.rolling(10).min())
-
-print("thing", sample.index.size)
 sectionsTemp =  sample.index.size / 10
 sections = int(sectionsTemp)
 print("sections", sections)
 
-#test = sample.iloc[1::10, :]
-#print("test", test)
-
-#print("Sample2", sample2)
-#print("Test", sample.loc[4:5])
-
-splitSections = []
-
-#newDf = pd.DataFrame(data, 30)
-#print("TEST", newDf)
+splitSections = {}
+list = []
 
 for x in range(sections):
+    """ This function takes the number of desired sections and creates a dataframe
+    for each one, adding each dataframe to a list """
     if x != 0:
         end = x*10
-        print("this is x", x*10)
         piece = sample.loc[end-10: end]
+        splitSections[x] = piece
         pd.DataFrame(piece)
-        splitSections.append(piece)
-        print("Inside loop", piece)
+        list.append(piece)
+
 else: 
     print("pass")
     pass
 
-print("working?", splitSections)
-    
+#print("working?", splitSections)
+#print("SOME", list)
+print("DFAD", splitSections[2])
+print("Another", splitSections[3])
 
-
-
-#for index, row in sample2.iterrows():
-    #print("inside function", row['Price'], row['Time (local)'])
-
-#chopper()
+#for x in splitSections:
+    #print("new", splitSections[x])
